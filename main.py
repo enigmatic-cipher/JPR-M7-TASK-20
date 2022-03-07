b33761ae75bba9e090d612c36d5c783705526cb8
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-03-07 06:23:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Given a number as input, convert it into a string representing the number in binary. Use recursion. Do not use loops.
+Input-> 99999
+Output-> 11000011010011111
+"""
+
+def recur(n):
+  
